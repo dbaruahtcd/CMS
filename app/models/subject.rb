dfeaf45011320e148496a5be6363f695aed19b55
dbaruah <baruahd@tcd.ie>
2021-04-15 20:16:36 +0100

    Added models for pages sections and users

---
+++ app/models/subject.rb
@@ -0,0 +1,8 @@
+class Subject < ApplicationRecord
+  scope :visible, -> { where(:visible => true) }
+  scope :invisible, -> { where(:visible => false) }
+  scope :sorted, -> { order("position ASC") }
+  scope :newest_first, -> { order("created_at DESC") }
+  # scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
+  scope :search, -> (query){where(["name LIKE ?", "%#{query}%"])}
+end
