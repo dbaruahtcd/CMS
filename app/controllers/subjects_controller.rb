291672632e598d581a02515412d9b2fa3c07cee0
dbaruah <baruahd@tcd.ie>
2021-04-16 23:15:53 +0100

    Added controllers and views for subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,25 +1,59 @@
 class SubjectsController < ApplicationController
+
   def index
+    @subjects = Subject.sorted
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new({name: 'Default'})
   end
 
   def create
+    # Create new object, Save object, redirect to index if success else redisplay form.
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      flash[:notice] = "Subject created successfully."
+      redirect_to(subjects_path)
+    else
+      # Render would just display the template not call the new action again and it would have the form
+      # pre populated.
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
-  def updated
+  def update
+    @subject = Subject.find(params[:id])
+
+    if @subject.update_attributes(subject_params)
+      flash[:notice] = "Subject updated successfully."
+      redirect_to(subject_path(@subject)) # show action
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "#{@subject.name} destroyed successfully."
+    redirect_to(subjects_path)
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
