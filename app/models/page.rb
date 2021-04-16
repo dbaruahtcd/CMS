7a48305c8cd97b38e9b59a4a68a8bc5d762e06e5
dbaruah <baruahd@tcd.ie>
2021-04-16 01:22:05 +0100

    Added associations and new model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,6 @@
 class Page < ApplicationRecord
+
+  belongs_to :subject, {optional: false }# , {:foreign_key => 'foreign_key'}
+  has_many :sections
+  has_and_belongs_to_many :admin_users, join_table: :admin_users_pages
 end
