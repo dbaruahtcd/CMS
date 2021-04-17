941d88084e73376ee0407caed9bdc72c17a178c0
dbaruah <baruahd@tcd.ie>
2021-04-18 00:31:41 +0100

    Added model validations

--- app/models/section.rb
+++ app/models/section.rb
@@ -8,4 +8,12 @@ class Section < ApplicationRecord
   scope :visible, -> { where(visible: true) }
   scope :invisible, -> { where(visible: false) }
   scope :newest_first, -> { order('created_at DESC') }
+
+  CONTENT_TYPES = ['text', 'HMTL']
+
+  validates_presense_of :name
+  validates_length_of :name, maximum: 255
+  validates_inclusion_of :content_type, in: CONTENT_TYPES,
+    message: "Must be one of: #{CONTENT_TYPE.join(', ')}"
+  validates_presence_of :content
 end
